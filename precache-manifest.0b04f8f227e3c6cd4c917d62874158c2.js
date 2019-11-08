7f8722d739ea4e8d58c34cd06fec480573da3148
Serhii_Y <dj.ser961@gmail.com>
2019-11-08 20:05:28 +0200

    Updates

---
+++ precache-manifest.0b04f8f227e3c6cd4c917d62874158c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72788a49f04cee04840794efd86f0c60",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c4f8248ce02a2b0a654f",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.c37b5d79.chunk.css"
+  },
+  {
+    "revision": "006d51ac014368be1739",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a285e9b3.chunk.css"
+  },
+  {
+    "revision": "c4f8248ce02a2b0a654f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3f286452.chunk.js"
+  },
+  {
+    "revision": "006d51ac014368be1739",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.2be4bdaa.chunk.js"
+  },
+  {
+    "revision": "a797996bce3a0f5447f5",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.8aee2e6f.js"
+  }
+]);
